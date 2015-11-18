f9d552a122a05f591951bea43ba569d9b9d54ebd
babassmine <bassco10@gmail.com>
2015-11-17 19:12:23 -0500

    ping sweeper get correct for command based on platform

--- ping_sweep.py
+++ ping_sweep.py
@@ -1,7 +1,15 @@
 import os
+import platform
 net = raw_input("Enter the Network Address ")
 st1 = int(raw_input("Enter Starting Number "))
 en1 = int(raw_input("Enter Ending Number "))
 
+oper = platform.system()
+if (oper == 'Windows'):
+	ping = 'ping -n 1'
+else:
+	ping = 'ping -c 1'
 
+for i in range(st1, en1):
+	
 
