768164610371b19b990d7d4e764a9e7e8f1c68db
babassmine <bassco10@gmail.com>
2015-11-17 19:24:35 -0500

    finish code but ping breaks

--- ping_sweep.py
+++ ping_sweep.py
@@ -1,5 +1,6 @@
 import os
 import platform
+from datetime import datetime
 net = raw_input("Enter the Network Address ")
 st1 = int(raw_input("Enter Starting Number "))
 en1 = int(raw_input("Enter Ending Number "))
@@ -10,6 +11,20 @@ if (oper == 'Windows'):
 else:
 	ping = 'ping -c 1'
 
-for i in range(st1, en1):
+t1 = datetime.now()
+for ip in range(st1, (en1+1)):
+	addr = net+'.'+str(ip)
+	comm = ping + addr
+	response = os.popen(comm)
+	for line in response.readline():
+		if (line.count('TTL')):
+			break
+		if (line.count('TTL')):
+			print addr, "----> LIVE"
+
+t2 = datetime.now()
+total = t2 - t1
+print "Scanning complete in ", total
+	
 	
 
