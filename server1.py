b4c91279903de7fb6f92162902c5143de9a203d6
babassmine <bassco10@gmail.com>
2015-11-20 23:38:26 -0500

    change server file

--- server1.py
+++ server1.py
@@ -2,7 +2,6 @@ import socket
 
 host = "10.0.0.167" # server address
 port =  12345 # port of server
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket
 s.bind((host,port)) #bind server - connects address to socket
 s.listen(2) # starts the tcp listener
