180e8d857a79abe2ed902075376b743e7398b4e7
babassmine <bassco10@gmail.com>
2015-11-22 12:50:45 -0500

    proper indentation

--- port_dictionary.py
+++ port_dictionary.py
@@ -25,28 +25,28 @@ def list1():
 	print "*"*30
 	shelf = shelve.open('abass.raj', writeback=True)
 	data = (shelf['desc'])
-	for key, value in data.items():
-		print key, ':', value
-		print '*'*30
-		print "\tProgram to Update or Add and Delete the port number detail\n"
-	while(1):
-		print "Press"
-		print "C for create inly one time create"
-		print "U for update or Add\nD for Delete\nL for List all values"
-		print "E for exit "
-		c = raw_input("Enter : ")
+for key, value in data.items():
+	print key, ':', value
+	print '*'*30
+	print "\tProgram to Update or Add and Delete the port number detail\n"
+while(1):
+	print "Press"
+	print "C for create inly one time create"
+	print "U for update or Add\nD for Delete\nL for List all values"
+	print "E for exit "
+	c = raw_input("Enter : ")
 
-	if (c=='C' or c=='c'):
-		create()
-	elif (c=='U' or c=='u'):
-		update()
-	elif(c=='D' or c=='d'):
-		del1()
-	elif(c=='L' or c=='l'):
-		list1()
-	elif(c=='E' or c=='e'):
-		exit()
-	else:
-		print "\tWrong Input"
+if (c=='C' or c=='c'):
+	create()
+elif (c=='U' or c=='u'):
+	update()
+elif(c=='D' or c=='d'):
+	del1()
+elif(c=='L' or c=='l'):
+	list1()
+elif(c=='E' or c=='e'):
+	exit()
+else:
+	print "\tWrong Input"
 
 
