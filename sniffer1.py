0b3eb05b70cc346e8873af6727614eed0dd539b4
babassmine <bassco10@gmail.com>
2015-11-28 19:59:33 -0500

    changed unpack function

--- sniffer1.py
+++ sniffer1.py
@@ -14,13 +14,17 @@ while True:
 
 	ipheader = pkt[0][14:34]
 
-	ip_hdr = struct.unpack("!12s4s4s", ipheader)
+	ip_hdr = struct.unpack("!8sB3s4s4s", ipheader)
 	print "---------------IP-------------------------"
-	print "Source IP", socket.inet_ntoa(ip_hdr[1])
-	print "Destination IP", socket.inet_ntoa(ip_hdr[2])
+	print "TTL :", ip_hdr[1]
+	print "Source IP", socket.inet_ntoa(ip_hdr[3])
+	print "Destination IP", socket.inet_ntoa(ip_hdr[4])
 	print "-------------TCP--------------------------"
 	tcpheader = pkt[0][34:54]
 	tcp_hdr = struct.unpack("!HH9ss6s", tcpheader)
 	print "Source Port ",  tcp_hdr[0]
 	print "Destination Port ", tcp_hdr[1]
 	print "Flag ", binascii.hexlify(tcp_hdr[3])
+	print "-------------------------------------------"
+	print pkt[0][54:]
+	print "-------------------------------------------"
